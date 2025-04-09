bb0c3b8017605ab99e6fda4432a2e999ff5504ba
yordi <tyordi09@gmail.com>
2025-04-09 17:40:32 +0300

    JUST THE FIBONACCI🤞

---
+++ chapter_three/Fibonacci.py
@@ -0,0 +1,14 @@
+class Solution:
+    def fib(self, n: int) -> int:
+        if n==0:
+            return 0
+        elif n==1:
+            return 1
+        else:
+             return self.fib(n-1)+self.fib(n-2)
+
+sol=Solution()
+result=sol.fib(2)
+print(result)
+        
+        
