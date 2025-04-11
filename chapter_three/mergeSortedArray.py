f89d388911dd47fa3b7016e758b5b456f1ac1838
yordi <tyordi09@gmail.com>
2025-04-11 09:32:19 +0300

    Merge the given array in a sorted form

---
+++ chapter_three/mergeSortedArray.py
@@ -0,0 +1,23 @@
+class Solution:
+    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+        i, j, k = m - 1, n - 1, m + n - 1 
+        while i >= 0 and j >= 0:
+            if nums1[i] > nums2[j]:
+                nums1[k] = nums1[i]
+                i -= 1
+            else:
+                nums1[k] = nums2[j]
+                j -= 1
+            k -= 1
+        while j >= 0:
+            nums1[k] = nums2[j]
+            j -= 1
+            k -= 1
+Number1 = [1,2,3,0,0,0]
+m = 3
+Number2 = [2,5,6]
+n = 3
+sol = Solution()
+sol.merge(Number1, m, Number2, n)
+print(Number1)
+      
